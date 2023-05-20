afea632b6772da92aa0c24272695244295a02e82
Anton Sakhanovych <antonsakhanovych@gmail.com>
2023-05-20 17:30:26 +0200

    correctly working website

--- api/server.js
+++ api/server.js
@@ -7,15 +7,22 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/backend', (req, res) => {
-  console.log(req.body.url, " ", req.body.keywords)
-  res.redirect('/')
+app.get('/results', (req, res) => {
+  res.render('results')
+})
+
+app.post('/results', (req, res) => {
+  let advertisements = [ {url: "https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg", valid: false},
+  {url: "https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000", valid: true}]
+  res.render('results', {advertisements})
 })
 
 
