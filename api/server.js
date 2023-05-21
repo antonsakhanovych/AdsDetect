babd0854f1662d25ecaa54148ebb98ed570df104
Anton Sakhanovych <antonsakhanovych@gmail.com>
2023-05-21 03:10:20 +0200

    create correct output of the result page

--- api/server.js
+++ api/server.js
@@ -15,13 +15,23 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/results', (req, res) => {
-  res.render('results')
-})
+// app.get('/results', (req, res) => {
+//   res.render('results')
+// })
 
 app.post('/results', (req, res) => {
-  let advertisements = [ {url: "https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg", valid: false},
-  {url: "https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000", valid: true}]
+  let advertisements = {"url": "https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg",
+     "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+     "context": "Kontekst przeglądarki",
+     "ads": {"name": "Unique name",
+      "destination_url":
+       ["https://tracking_url_1.pl",
+        "https://tracking_url_2.pl",
+        "https://firmaxyz.pl/produkt"],
+      "words": ["Firma Inwestcyjna", "Produkt"],
+      "screenshot_ads": ["https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+       "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",]
+    }}
   res.render('results', {advertisements})
 })
 
